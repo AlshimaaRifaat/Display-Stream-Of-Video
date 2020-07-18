b7b4df9121fac5854dbfe3029dafbd7a75c8ee9d
AlshimaaRifaat <36330776+AlshimaaRifaat@users.noreply.github.com>
2020-07-18 16:17:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/displaystreamofvideo/Config.java
@@ -0,0 +1,6 @@
+package com.example.displaystreamofvideo;
+
+public class Config {
+    public static final String DEVELOPER_KEY = "AIzaSyDXwLqOCVzH5DNbVEEwwO5N8yKvYyGgC9Y";
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
